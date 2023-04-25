8c3340e226db0aa5de2199831a82353c7e0c1b82
mavedirra-01 <mavedirra@gmail.com>
2023-04-25 21:33:37 +0000

    d

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ class Container:
             return output.decode('utf-8').strip()
         except subprocess.CalledProcessError as e:
             a = e.output.decode('utf-8')
-            print("here:", a)
+            print("here:", output)
             # Ignore the error message and return "exited" if container is not running
             if "template parsing error" in e.output.decode('utf-8'):
                 return "exited"
