22fb7cb8b1c64df99e4b4ce857a46434675b0e93
mavedirra-01 <mavedirra@gmail.com>
2023-04-26 01:44:11 +0000

    g

--- app.py
+++ app.py
@@ -63,9 +63,8 @@ def reset_container(container_name):
 
 @app.route('/terminal/<container_name>')
 def terminal(container_name):
-    # port = random.randint(10001, 65535)
-    port = '18292'
-    ttyd_command = f"ttyd -p {port} docker exec -it /bin/bash {container_name}"
+    port = random.randint(10001, 65535)
+    ttyd_command = f"ttyd -p {port} docker exec -it {container_name} /bin/bash"
     subprocess.Popen(ttyd_command.split())
     return render_template('index.html', container_name=container_name, port=port, container=containers[container_name])
 
