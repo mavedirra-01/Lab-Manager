46e2c9aea30b96d95bb32708cb46f9b5feb5c0a7
mavedirra-01 <mavedirra@gmail.com>
2023-04-27 19:30:59 +0000

    d

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def terminal(container_name):
     return redirect(f"http://192.168.2.136:{port}")
 
 
-@app.route('/containers')
+@app.route('/containers_status')
 def get_containers_status():
     global containers
     containers_status = {}
