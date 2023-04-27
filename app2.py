752ff267f2769245679a43c8209f0b39a6537e36
mavedirra-01 <mavedirra@gmail.com>
2023-04-27 19:47:34 +0000

    idk

--- app2.py
+++ app2.py
@@ -1,31 +1,120 @@
-from flask import Flask, render_template, request
 import docker
+import time
+import random
+import json
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
 client = docker.from_env()
 
 
-app = Flask(__name__)
+class Container:
+    def __init__(self, name, image):
+        self.name = name
+        self.status = self.get_status()
+        self.image = image
 
-@app.route('/start', methods=['POST'])
-def start_container():
-    container_id = request.form['id']
-    container = client.containers.get(container_id)
-    container.start()
-    return 'Container started'
+    def get_status(self):
+        try:
+            container = client.containers.get(self.name)
+            return container.status
+        except docker.errors.NotFound:
+            return "Not started"
+
+    def start(self):
+        try:
+            client.containers.run(self.image, detach=True,
+                                  hostname=self.name, name=self.name)
+        except docker.errors.APIError:
+            pass
+
+    def remove(self):
+        try:
+            container = client.containers.get(self.name)
+            container.remove(force=True)
+        except docker.errors.NotFound:
+            pass
+
+    def stop(self):
+        try:
+            container = client.containers.get(self.name)
+            container.stop()
+        except docker.errors.NotFound:
+            pass
+
+    def reset(self):
+        self.stop()
+        time.sleep(2)
+        self.start()
+
+    def update_containers(self):
+        global containers
+        containers_status = {}
+        for container in client.containers.list(all=True):
+            if self.name == container.name:
+                self.status = container.status
+                break
+        if self.name not in containers:
+            containers[self.name] = self
 
 
-@app.route('/stop', methods=['POST'])
-def stop_container():
-    container_id = request.form['id']
-    container = client.containers.get(container_id)
-    container.stop()
-    return 'Container stopped'
+containers = {}
 
+for container in client.containers.list(all=True):
+    containers[container.name] = Container(
+        container.name, container.image.tags[0])
+
+
+# Define routes for starting, stopping, and resetting containers
+@app.route('/start_container/<container_name>', methods=['POST'])
+def start_container(container_name):
+    if container_name in containers:
+        containers[container_name].start()
+    return redirect(url_for('index'))
+
+
+@app.route('/stop_container/<container_name>', methods=['POST'])
+def stop_container(container_name):
+    if container_name in containers:
+        containers[container_name].stop()
+    time.sleep(0.5)
+    return redirect(url_for('index'))
+
+
+@app.route('/reset_container/<container_name>', methods=['POST'])
+def reset_container(container_name):
+    if container_name in containers:
+        containers[container_name].reset()
+    return redirect(url_for('index'))
+
+
+@app.route('/terminal/<container_name>/', methods=['POST'])
+def terminal(container_name):
+    port = random.randint(10001, 65535)
+    ttyd_command = f"ttyd -p {port} docker exec -it {container_name} /bin/bash"
+    subprocess.Popen(ttyd_command.split())
+    time.sleep(1)
+    return redirect(f"http://192.168.2.136:{port}")
+
+
+@app.route('/containers_status')
+def get_containers_status():
+    global containers
+    containers_status = {}
+    for container_name, container in containers.items():
+        container.update_containers()
+        containers_status[container_name] = container.status
+    return json.dumps(containers_status)
 
 
 @app.route('/')
 def index():
-    containers = client.containers.list()
-    return render_template('index2.html', containers=containers)
+    global containers
+    for container in client.containers.list(all=True):
+        if container.name not in containers:
+            containers[container.name] = Container(
+                container.name, container.image.tags[0])
+    return render_template('index.html', containers_list=containers)
 
 
 # Define a class for managing VMs
