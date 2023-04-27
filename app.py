15d8fd026f09677732d3e76c8bdc43d1f793a0b7
mavedirra-01 <mavedirra@gmail.com>
2023-04-27 16:54:39 +0000

    d

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def update_containers_endpoint():
         containers_status[name] = {
             'status': container.status
         }
-    return render_template('index.html', containers=containers_status, containers_list=container_manager.containers)
+    return redirect(url_for('index'))
 
 
 
