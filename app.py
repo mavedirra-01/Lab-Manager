686077443464217b80d24f0605b1128e686ea2c5
mavedirra-01 <mavedirra@gmail.com>
2023-04-27 16:13:43 +0000

    d

--- app.py
+++ app.py
@@ -24,7 +24,6 @@ class ContainerManager:
                 self.containers[name] = Container(name, image)
             else:
                 self.containers[name].image = image
-                # self.containers[name].status = status
 
     def update_containers_thread(self):
         self.update_containers()
